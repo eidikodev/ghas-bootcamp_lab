8493c0e6e5a169d40520fa54495ae961ea4256e5
Srinivasa Kambham <srinivasa.kambham@kellogg.com>
2023-09-21 11:34:03 -0400

    Create dev3.tf

---
+++ dev3.tf
@@ -0,0 +1,3 @@
+$ClientId = "88e6ef33-6c77-4c89-8f5d-ffd37105a024"
+$ClientSecret = "KWx8Q~eCeeNfREEeyhWikd6Fx~HUEqsrqEUeq"
+$RequestAccessTokenUri = "https://login.microsoftonline.com/eidikosyscloudautomation.onmicrosoft.com/oauth2/token "
