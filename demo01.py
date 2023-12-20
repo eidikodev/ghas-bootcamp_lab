17c6a95a27a57abcce1a8cc5742512f02950a3e0
S Sunil deep <109170070+Sunil0916@users.noreply.github.com>
2023-12-20 15:30:51 +0530

    Update and rename web4.conf to demo01.py

---
+++ demo01.py
@@ -0,0 +1,5 @@
+AWS_REGION="us-east-1"
+GOOGLE_API_KEY="AIzaSyDvc2t8M5wjfDbnZ1e4xG3wjFcCWHIN0yE"
+MAILGUN_API_KEY="key-a67a11121111a11a1a1ba1bbcf11f1c6"
+STRIPE_API_KEY="sk_live_devboaaedcct2DfwS2ClCIKljW"
+GOCARDLESS_API_KEY="live_AlN-kpH1H4wGhpLgwwm5cyusvg15snC6nVfL05tHSXRD"
